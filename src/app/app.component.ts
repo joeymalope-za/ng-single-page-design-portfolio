42171ac94381487645b65f66b7097580359589fa
joeymalope-za <94571003+Joemalope@users.noreply.github.com>
2022-07-24 15:37:40 +0200

    carousel integration 🎡

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component,ElementRef,ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'ng-single-page-design-portfolio';
+  @ViewChild('myCarousel', { static: false })
+  myCarousel!: any;
+
+  title = 'Single Page Portfolio';
+  slides:number; 
+
+  constructor(){
+    this.slides = (window.innerWidth > 575)? 3 : 1;
+  }
+
+  next(){
+    this.myCarousel.next();
+  }
+
+  prev(){
+    this.myCarousel.prev();
+  }
 }
